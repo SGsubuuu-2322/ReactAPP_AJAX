4686f6055f9f45eacb057e3ff36a6f5f91040dd4
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-03-09 21:37:51 +0530

    Learned and Implemented reacts useEffect and its features for re-rendering and updating the component...

--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -1,16 +1,36 @@
 // import React from 'react'
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const Services = () => {
+  const [first, setFirst] = useState("This is normal data...");
+  const [second, setSecond] = useState("This is important data...");
   useEffect(() => {
     console.log("Component created successfully...");
 
     return () => {
       console.log("Component deleted successfully");
     };
-  });
-  return <div>Services</div>;
+  }, [second]);
+  return (
+    <div>
+      <h1>{first}</h1>
+      <button
+        onClick={() => setFirst("Normal data has been changed...")}
+        className="px-3 py-2 text-xl font-semibold bg-red-500 rounded-md mb-4"
+      >
+        Change Normal Data
+      </button>
+
+      <h1>{second}</h1>
+      <button
+        onClick={() => setSecond("Important data has been changed...")}
+        className="px-3 py-2 text-xl font-semibold bg-red-500 rounded-md"
+      >
+        Change Normal Data
+      </button>
+    </div>
+  );
 };
 
 export default Services;
