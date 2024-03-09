9399963e0e4996cbe957724e14d5647827da2fa7
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-03-09 18:02:29 +0530

    Learned and Implemented get api with axios in our app...

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
 // import React from 'react'
+import axios from "axios";
 
 const App = () => {
-  return <div className="text-3xl font-semibold">App</div>;
+  const getData = () => {
+    const api = "https://fakestoreapi.com/products";
+
+    axios
+      .get(api)
+      .then((products) => console.log(products))
+      .catch((err) => console.log(err));
+  };
+  return (
+    <div className="text-3xl font-semibold mt-5 ml-5">
+      <button
+        onClick={getData}
+        className="px-3 py-2 text-xl font-semibold bg-red-500 rounded-md"
+      >
+        Get Data
+      </button>
+    </div>
+  );
 };
 
 export default App;
